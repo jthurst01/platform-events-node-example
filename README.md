78b324f37930bf2132031f683b934b0ede84feb1
Jay Hurst <jhurst@salesforce.com>
2017-03-23 18:06:11 -0700

    New version 1

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-platform-event-servicemax
+platform-event-socketio
 =======================
 
 Node demo that streams Platform Events from Salesforce.com to the browser using socket.io. You can run the demo by opening:
 
-[platform-event-servicemax demo](http://platform-event-servicemax.herokuapp.com/) - New Platform Events are streamed to this app and display in the browser using socket.io.
+[platform-event-socketio demo](http://platform-event-socketio.herokuapp.com/) - New Platform Events are streamed to this app and display in the browser using socket.io.
 
 ### Setup Remote Access in Salesforce.com
 
@@ -16,8 +16,8 @@ Create a new Platform Event from the Setup Menu of your Salesforce Instance.
 ### Running the Application Locally
 
 ```
-git clone https://github.com/jthurst/platform-event-servicemax.git
-cd platform-event-servicemax
+git clone https://github.com/jthurst/platform-event-socketio.git
+cd platform-event-socketio
 npm install
 ```
 
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,18 @@ var router = express.Router();
 var Promise = require("bluebird");
 
 var nforce = require('nforce');
-var org = require('../app');
+var app = require('../app');
 
 const util = require('util');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Return Request Demo' });
+});
+
+/* GET home page. */
+router.get('/cdc', function(req, res, next) {
+	res.render('cdc', { title: 'Change Data Events Demo' });
 });
 
 /* Creates a new the record */
@@ -22,7 +27,51 @@ router.post('/', function(req, res) {
 	newEvent.set('Serial_Number__c', req.body.serialNumber);
 	newEvent.set('Issue_Date__c', req.body.issueDate);
 	newEvent.set('Issue_Description__c', req.body.issueDescription);  
-	org.org.insert({ sobject: newEvent })
+	app.org.insert({ sobject: newEvent })
+	res.sendStatus(200);
+});
+
+router.post('/newSub', function(req, res) {
+	var cj = app.org.createStreamClient({ topic: app.config.TOPIC, replayId: app.config.REPLAY_ID });
+	var str = cj.subscribe({ topic: app.config.TOPIC, oauth: app.oauth });
+	//console.log(util.inspect(cj, false, null));
+
+	str.on('connect', function(){
+		console.log('Connected to topic: ' + app.config.TOPIC);
+	});
+
+	str.on('error', function(error) {
+		console.log('Error received from topic: ' + error);
+	});
+
+	str.on('data', function(data) {
+		console.log('Received the following from topic ---');
+		console.log(data);
+		// emit the record to be displayed on the page
+		app.socket.emit('event-processed', JSON.stringify(data));
+	});
+	res.sendStatus(200);
+});
+
+router.post('/newCDCSub', function(req, res) {
+	var cj = app.org.createStreamClient({ topic: '/data/LeadChangeManualEvent', replayId: -2 });
+	var str = cj.subscribe({ topic: '/data/LeadChangeManualEvent', oauth: app.oauth });
+	//console.log(util.inspect(cj, false, null));
+
+	str.on('connect', function(){
+		console.log('Connected to topic: ' + '/data/LeadChangeManualEvent');
+	});
+
+	str.on('error', function(error) {
+		console.log('Error received from topic: ' + error);
+	});
+
+	str.on('data', function(data) {
+		console.log('Received the following from topic ---');
+		console.log(data);
+		// emit the record to be displayed on the page
+		app.socket.emit('event-processed', JSON.stringify(data));
+	});
 	res.sendStatus(200);
 });
 
